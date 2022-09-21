80b91cb1d10b4e77653543a333f91b547a886a92
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-09-22 00:17:03 +0300

    react-hooks-Vladilen  закончен useRef. 26мин

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import RefHook from "./components/RefHook";
+import stateHook from "./components/StateHook";
 
+
+function complexCompute(num) {
+    let i = 0;
+    while (i < 1000000000) i++
+        return num * 2;
+}
 
 function App() {
+    const [number, setNumber] = useState(42);
+
+
+    const computed = complexCompute(number)
 
     console.log('App loaded');
     return (
@@ -12,7 +23,11 @@ function App() {
                 {/*<StateHook/>*/}
                 {/*<Robot/>*/}
                 {/*<EffectHook/>*/}
-                <RefHook/>
+                {/*<RefHook/>*/}
+                <h1>Вычисляемое свойство: {computed}</h1>
+
+                <button className="btn btn-outline-success" onClick={() => setNumber(prevState => prevState + 1)}><b>Добавить</b></button>
+                <button className="btn btn-outline-danger" onClick={() => setNumber(prevState => prevState - 1)}><b>Убрать</b></button>
             </div>
         </div>
     );
