a5daaa3020271cb3bdc3b0de2688e000695f4d1e
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-09-26 12:38:56 +0300

    react-hooks-Vladilen  не закончен UseCallbackHook.js. 1ч 6миню

--- src/components/UseCallbackHook.js
+++ src/components/UseCallbackHook.js
@@ -1,21 +1,28 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect, useMemo} from 'react';
 
 const UseCallbackHook = () => {
 	const [count, setCount] = useState(1);
 	const [colored, setColored] = useState(false);
 
-	const styles =  {color: colored ? 'black' : 'red'};
+	const styles = useMemo(() => ({color: colored ? 'black' : 'red'}), [colored]);
+
 	
-	const generateItemsFromAPI = () => {
-	    return new Array(count).fill('').map((_, i) => 'элемент ' + (i + 1));
-	}
+	const generateItemsFromAPI = useCallback(() => {
+			return new Array(count).fill('').map((_, i) => 'элемент ' + (i + 1));
+		},
+		[count],
+	);
+
 
 	return (
 		<div>
 			<h1 style={styles}>Вычисляемое свойство: {count}</h1>
 
 			<button className="btn btn-outline-success" onClick={() => setCount(prevState => prevState + 1)}><b>Добавить</b></button>
-			<button className="btn btn-outline-warning" onClick={() => setColored(prevState => !prevState)}>изменить цвет</button>
+			<button className="btn btn-outline-warning" onClick={() => setColored(prevState => {
+				console.log('color')
+				return !prevState;
+			})}>изменить цвет</button>
 			<ListGenerated generateItemsFromAPI={generateItemsFromAPI}/>
 		</div>
 	);
@@ -25,12 +32,20 @@ export default UseCallbackHook;
 
 
 export const ListGenerated = ({generateItemsFromAPI}) => {
+	const [items, setItems] = useState([])
+	useEffect(() => {
+		const newItems = generateItemsFromAPI();
+
+		setItems(newItems);
+		console.log('render');
+	}, [generateItemsFromAPI]);
+
 	return (
-		<div>
+		<>
 			<ul>
-				{generateItemsFromAPI().map((item, i) => <li key={i}>{item}</li>)}
+				{items.map((item, i) => <li key={i}>{item}</li>)}
 			</ul>
-		</div>
+		</>
 	);
 };
 
